5341f75a7230985956c91109b56343a5fde5aca1
IONEKIS <ivankiselev660@gmail.com>
2025-05-14 05:49:31 +0300

    Initial Commit

---
+++ TESTTASK/TESTTASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTTASKApp.swift
+//  TESTTASK
+//
+//  Created by Ivan Kisilov on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TESTTASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
